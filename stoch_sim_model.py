8c0152a9d38a9524308dcdde9d44788238fc520a
Obinna Ukogu <ObinnaUkogu@macbook-pro-131.lan>
2022-10-08 14:43:03 -0700

    Updated noise models

--- stoch_sim_model.py
+++ stoch_sim_model.py
@@ -11,13 +11,13 @@ import numba as nb
 # Define simulation parameters
 
 # antigens
-a1_0 = 100_000
+a1_0 = 100
 a2 = 50_000_000
 a1_max = 0.4*a2
 b_a1 = 10
 t1 = 10
 d_a1 = 3.8*10**(-4) # proxy for virulence
-T_a1 = 10
+T_a1 = 2
 
 t2 = 0.5 # dissociation time
 n, m = 4, 2
@@ -29,7 +29,7 @@ g_NE_max = 0 #0.36
 g_NM_max = 4 #0.12
 b_E_max = 2.0
 g_ME_max = 6.0
-g_EM_max = 0.02
+g_EM_max = 0.07
 
 k_E = 10**(-11)*(6.0221408*10**23)*50*10**(-6)
 k_M = k_E
@@ -47,10 +47,10 @@ I_c1 = 0
 I_c2 = 0
 
 # set initial state
-init_state = np.array([a1_0, N0, 0, 0, 0, Treg0]) #a1, N, E, cM, eM, T
+init_state = np.array([N0, 0, 0, 0, Treg0]) # N, E, cM, eM, T
 
 # activation threshold
-mu_theta = 5_000_000
+mu_theta = 100_00 # set to point at which activation probabity equals antigen frequency
 sigma_theta = np.log((mu_theta*t1**n +t2**n)/(mu_theta*t1**m + t2**m))/10
 
 ### (2) Define functions for simulations
@@ -78,9 +78,9 @@ def c2_ss(a1, E):
     return E*b_c2*tau_c
 
 @nb.njit
-def c1_ss(t1,a1, E, T):
+def c1_ss(t1,a1, E, T, b_c1_pop = b_c1):
     
-    return (E*b_c1*p_A(t1,a1) + I_c1)/(T*f_T + 1/tau_c)
+    return (E*b_c1_pop*p_A(t1,a1) + I_c1)/(T*f_T + 1/tau_c)
 
 @nb.njit
 def g_NE(T):
@@ -108,14 +108,14 @@ def g_ME(t1,a1,E,T):
     return g_ME_max*p_A(t1,a1)
 
 @nb.njit
-def g_EM(T):
+def g_EM(t1,a1,E,T):
     
-    return g_EM_max
+    return g_EM_max*(1-p_A(t1,a1))
 
 @nb.njit
-def d_E(t1,a1, E, T, b_E_pop = b_E_max):
+def d_E(t1,a1, E, T, b_E_pop = b_E_max, b_c1_pop = b_c1):
     
-    return (b_E_pop + d_E_max)*(1-hl_u(c1_ss(t1,a1, E, T), k_E))
+    return (b_E_pop + d_E_max)*(1-hl_u(c1_ss(t1,a1, E, T, b_c1_pop), k_E))
 
 @nb.njit
 def d_T(T):
@@ -124,35 +124,29 @@ def d_T(T):
 
 # define dynamics
 @nb.njit
-def state_dyn(t, z, b_a1, t1, d_a1, T_a1):
+def state_dyn(t, z, a1_0, b_a1, t1, d_a1, T_a1):
     
     
     a1, N, E, cM, eM, T = z
     
-    #P = P1(a1,E)[0] # set to 0 for noiseless, 1 for noise
-    #P_vec[np.argmax(P_vec == -999)] = P # (1) uncomment to debug noise
-    
-    return np.asarray([a1*b_a1*(1- a1/a1_max - t/T_a1) - d_a1*a1*E,\
+    return np.asarray([a1*b_a1*(1- a1/a1_max)*(1 - (a1*b_a1*(1 - a1/a1_max) - d_a1*a1*E <= 0)*(a1 <= mu_theta))*(a1 >= a1_0) - d_a1*a1*E,\
             -(g_NE(T)+g_NM(T))*N*p_A(t1,a1),\
-            g_NE(T)*N*p_A(t1,a1) + cM*g_ME(t1,a1,E,T) +  E*(b_E(T) - g_EM(T) - d_E(t1,a1,E,T)), \
+            g_NE(T)*N*p_A(t1,a1) + cM*g_ME(t1,a1,E,T) +  E*(b_E(T) - g_EM(t1,a1,E,T) - d_E(t1,a1,E,T)), \
             g_NM(T)*N*p_A(t1,a1) - cM*g_ME(t1,a1,E,T), \
-            E*g_EM(T),\
+            E*g_EM(t1,a1,E,T),\
             0*T*(b_T(T) - d_T(T))])
 
 @nb.njit
-def pop_state_dyn(t, z, b_a1, t1, d_a1, T_a1, b_E_pop, g_ME_pop):
+def pop_state_dyn(t, z, a1_0, b_a1, t1, d_a1, b_E_pop, g_ME_pop, b_c1_pop, T_a1 = T_a1):
     
     
     a1, N, E, cM, eM, T = z
     
-    #P = P1(a1,E)[0] # set to 0 for noiseless, 1 for noise
-    #P_vec[np.argmax(P_vec == -999)] = P # (1) uncomment to debug noise
-    
-    return np.asarray([a1*b_a1*(1- a1/a1_max)*(a1 >= a1_0) - d_a1*a1*E,\
+    return np.asarray([a1*b_a1*(1- a1/a1_max)*(1 - (a1*b_a1*(1 - a1/a1_max) - d_a1*a1*E <= 0)*(a1 <= mu_theta))*(a1 >= a1_0) - d_a1*a1*E,\
             -(g_NE(T)+g_NM(T))*N*p_A(t1,a1),\
-            g_NE(T)*N*p_A(t1,a1) + cM*g_ME_pop*p_A(t1,a1) +  E*(b_E_pop - g_EM(T) - d_E(t1,a1,E,T,b_E_pop)), \
-            g_NM(T)*N*p_A(t1,a1) - cM*g_ME_pop*p_A(t1,a1), \
-            E*g_EM(T),\
+            g_NE(T)*N*p_A(t1,a1) + cM*g_ME_pop*p_A(t1, a1) +  E*(b_E_pop - g_EM(t1,a1,E,T) - d_E(t1, a1, E, T, b_E_pop, b_c1_pop)), \
+            g_NM(T)*N*p_A(t1, a1) - cM*g_ME_pop*p_A(t1, a1), \
+            E*g_EM(t1, a1, E, T),\
             0*T*(b_T(T) - d_T(T))])
 
 ### (3) Code to run individual simulation
@@ -160,21 +154,47 @@ def pop_state_dyn(t, z, b_a1, t1, d_a1, T_a1, b_E_pop, g_ME_pop):
 duration = 20
 steps = 10**4
 
-def stoch_sim(b_a1 = 2, t1 = 10, d_a1 = 3.8*10**(-4), T_a1 = 10, noise_model = "pop", noise_cv = [0.5,0.5]):
+def stoch_sim(a1_0 = a1_0, b_a1 = b_a1, t1 = t1, d_a1 = d_a1, T_a1 = T_a1, noise_model = "pop", noise_cv = [0.0, 0.0, 0.0]):
 
     
     dt = duration/steps
     ts = np.linspace(0, duration, steps + 1)
+    
     if noise_model == "pop":
         b_E_pop = np.random.lognormal(mean = np.log(b_E_max), sigma = noise_cv[0]*np.abs(np.log(b_E_max)))
         g_ME_pop = np.random.lognormal(mean = np.log(g_ME_max), sigma = noise_cv[1]*np.abs(np.log(g_ME_max)))
-    
-        states = solve_ivp(pop_state_dyn, [0, duration], init_state,
-                    dense_output=True, args=[b_a1, t1, d_a1, T_a1, b_E_pop, g_ME_pop]).sol(ts).T
+        b_c1_pop = np.random.lognormal(mean = np.log(b_c1), sigma = noise_cv[2]*np.abs(np.log(b_c1)))
+        
+        states = solve_ivp(pop_state_dyn, [0, duration], np.concatenate(([a1_0], init_state), axis = None),
+                    dense_output=True, args=[a1_0, b_a1, t1, d_a1, b_E_pop, g_ME_pop, b_c1_pop, T_a1,]).sol(ts).T
     else:
-        states = solve_ivp(state_dyn, [0, duration], init_state,
-                    dense_output=True, args=[b_a1, t1, d_a1, T_a1]).sol(ts).T
+        states = solve_ivp(state_dyn, [0, duration], np.concatenate(([a1_0],init_state), axis = None),
+                    dense_output=True, args=[a1_0, b_a1, t1, d_a1, T_a1]).sol(ts).T
 
     a1, N, E, cM, eM, T = states[:,0], states[:,1], states[:,2], states[:,3], states[:,4], states[:,5]
     
     return a1, N, E, cM, eM, T, ts
+
+### (4) Parallelize simulation runs
+def sum_sim(a1_0 = a1_0, b_a1 = b_a1, t1 = t1, d_a1 = d_a1, T_a1 = T_a1, noise_cv = [0.1,0.0,0.0]):
+    # compute state and costate dynamics
+    dt = duration/steps
+    a1, N, E, cM, eM, T, ts = stoch_sim(a1_0, b_a1, t1, d_a1, T_a1, noise_cv = noise_cv)
+    run_data = [a1_0, b_a1, t1, d_a1, np.sum(a1*dt), np.argmax(a1)*dt, np.max(E)/(np.argmax(E)*dt), \
+                np.argmax(E)*dt, (eM[-1]+cM[-1])/np.max(E), np.sum(E*dt)]
+    return run_data
+
+stat_names = [r"$a_1^0$", r"$b_{a_1}$",r"$t_1$",r"$d_{a_1}$",r"$\int a_1 dt$",\
+                             r"$T_{a_1}^{max}$", r"$\frac{E^{max}}{T_{E}^{max}}$",r"$T_{E}^{max}$",\
+                             r"$\frac{(cM + eM)^\infty}{E^{max}}$",r"$\int E dt$"]
+
+### (5) define basic mutual information function
+from sklearn.metrics import mutual_info_score
+
+def calc_MI(x, y):
+    bx = np.histogram_bin_edges(x, bins="auto", range=None, weights=None)
+    by = np.histogram_bin_edges(y, bins="auto", range=None, weights=None)
+    
+    c_xy = np.histogram2d(x, y, [bx,by])[0]
+    mi = mutual_info_score(None, None, contingency=c_xy)/np.log(2)
+    return mi
