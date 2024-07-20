62af4f3712aaf87596edaa4d4d717b72067309df
OBINNA ADRIAN UKOGU <oukogu@klone-login03.hyak.local>
2024-07-20 14:04:39 -0700

    Additional refinements.

--- .ipynb_checkpoints/optimize_nets-checkpoint.py
+++ .ipynb_checkpoints/optimize_nets-checkpoint.py
@@ -10,7 +10,7 @@ from stoch_sim_model import *
 num_cpu = 30 # number of CPUs requested
 run_time = 4.0
 
-def run(batch = 0, outdir='', comment = "Nact-Ediv-vir", inf_sample = vir_prop, runs = 1, infection_type = 'prim', vir_model = "indep_harm", default_reg = act_psis + NM_psis + EM_psis + exp_psis, num_cpu = num_cpu):
+def run(batch = 0, outdir='', comment = "auto-Nact-Ediv-vir", inf_sample = vir_prop, runs = 1, infection_type = 'prim', vir_model = "indep_harm", default_reg = act_psis + NM_psis + EM_psis + exp_psis, num_cpu = num_cpu):
     
     # Run simulations over different infections
     if "auto" in comment:
