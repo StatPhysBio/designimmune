bd10f8845ac33b955d3759b6cfa1835b35e2fcf7
OBINNA ADRIAN UKOGU <oukogu@klone-login03.hyak.local>
2025-06-10 08:47:45 -0700

    Updated clustering.

--- .ipynb_checkpoints/run_over_regs-checkpoint.sh
+++ .ipynb_checkpoints/run_over_regs-checkpoint.sh
@@ -3,11 +3,11 @@ script_dir=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 max_job=498 #2814 # change depending on number of inputs
 # if some jobs don't run, run the following:
-# rerun_list=(146)
+rerun_list=(355)
 
-# for i in "${rerun_list[@]}";
+for i in "${rerun_list[@]}";
 
-for ((i=0; i<=${max_job}; i++));
+# for ((i=0; i<=${max_job}; i++));
 
 do
     sbatch ${script_dir}/submit_app_job.sh "${script_dir}/submit_optimize_nets.sh $i"
